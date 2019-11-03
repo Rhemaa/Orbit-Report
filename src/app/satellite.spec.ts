a80089564821b8108051ac9fddfe0a524d804418
Rhema Anazonwu <ruanazonwu@gmail.com>
2019-11-03 14:28:38 -0600

    updated with satellite class

---
+++ src/app/satellite.spec.ts
@@ -0,0 +1,19 @@
+// import { Satellite } from './satellite';
+
+class Satellite {
+  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    this.name = name;
+    name: "string";
+    this.orbitType = orbitType;
+    orbitType: "string";
+    this.type = type
+    type: "string";
+    this.operational = operational;
+    operational: "boolean";
+    this.launchDate = launchDate;
+    launchDate: "string";
+  }
+
+}
+
+export Satellite './satellite';
