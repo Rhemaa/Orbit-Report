3a8f3d1a22b23401928ee5b915089f0fbc1bfa83
Rhema Anazonwu <ruanazonwu@gmail.com>
2019-11-03 15:28:54 -0600

    added interface to satellite class

--- src/app/satellite.spec.ts
+++ src/app/satellite.spec.ts
@@ -1,19 +1,29 @@
 // import { Satellite } from './satellite';
 
-class Satellite {
+interface SatelliteInterface {
+  name: string;
+  orbitType: string;
+  type: string;
+  operational: boolean;
+  launchDate: string;
+}
+
+class Satellite implements SatelliteInterface{
+  name: string;
+  orbitType: string;
+  type: string;
+  operational: boolean;
+  launchDate: string;
   constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name;
-    name: "string";
     this.orbitType = orbitType;
-    orbitType: "string";
     this.type = type
-    type: "string";
     this.operational = operational;
-    operational: "boolean";
     this.launchDate = launchDate;
-    launchDate: "string";
   }
 
 }
 
-export Satellite './satellite';
+export class Satellite {
+  
+}
